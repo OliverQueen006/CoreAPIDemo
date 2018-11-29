18febb7a9b102f792903a826b1a8c6a61645e974
Hiten Pandya <hiten@kaushalaminc.com>
2018-11-29 18:13:26 +0530

    29-Nov-2018-Version-0.1

--- CoreAPI/WebAPIDemo/Models/UserModel.cs
+++ CoreAPI/WebAPIDemo/Models/UserModel.cs
@@ -6,8 +6,6 @@ namespace WebAPIDemo.Models
     [DataContract]
     public class UserModel
     {
-<<<<<<< HEAD
-
         [DataMember(Name = "Id")]
         public int Id { get; set; }
 
@@ -24,24 +22,6 @@ namespace WebAPIDemo.Models
         public string Address { get; set; }
         
         [DataMember(Name = "IsActive")]
-=======
-        [DataMember(Name = "Id")]
-        public int ID { get; set; }
-
-        [DataMember(Name = "Name")]
-        public string Name { get; set; }
-
-        [DataMember(Name = "EmailId")]
-        public string EmailID { get; set; }
-
-        [DataMember(Name = "Mobile")]
-        public string Mobile { get; set; }
-
-        [DataMember(Name = "Address")]
-        public string Address { get; set; }
-
-        [DataMember(Name = "IsActives")]
->>>>>>> a34019a681f9bf25975504cf86c1a20d00b56f2b
         public bool IsActive { get; set; }
     }
 }
